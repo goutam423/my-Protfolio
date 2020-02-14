4a16c8ceb507853c8e5c317705bb92e954534058
GOUTAM REDDY <goutamreddy423@gmail.com>
2020-02-14 17:54:32 +0530

    initial commit

---
+++ src/resumeData.js
@@ -0,0 +1,94 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Goutam Reddy",
+    "role": "student",
+    "roleDescription": "like to learn about new technologies, simply play games in my free time.",
+    "socialLinks":[
+        {
+          "name":"goutam423",
+          "url":"https://github.com/goutam423",
+          "className":"fa fa-github"
+        },
+      ],
+    "aboutme":"I am in KLE PC JABINS in HUBLI, Persuing BCA",
+    "address":"India",
+    "website":"https://github.com/goutam423",
+    "education":[
+      {
+        "UniversityName":"KLE BCA",
+        "specialization":" ",
+        "MonthOfPassing":"mar",
+        "YearOfPassing":"2021",
+        "Achievements":""
+      },
+      {
+        "UniversityName":"Some University",
+        "specialization":"Some specialization",
+        "MonthOfPassing":"Jan",
+        "YearOfPassing":"2018",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
